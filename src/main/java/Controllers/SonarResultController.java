5d41b7fbea4c2e55c42af4529d6c9991bd285964
djaka <djaka>
2016-05-01 05:13:39 +0200

    updated add issues to db

--- src/main/java/Controllers/SonarResultController.java
+++ src/main/java/Controllers/SonarResultController.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Main.Issue;
+import Main.JsonReader;
 import Main.Project;
-import Main.SonarPersister;
 import Main.User;
 
 @RestController
@@ -27,11 +29,53 @@ public class SonarResultController {
     @Autowired
     Main.SonarResultRepository sonarRepo;
     
+    private final String issuesUurl = "http://145.24.222.130:9000/api/issues/search?";    
 	
 	@RequestMapping("/updatesonardata")
 	public void UpdateSonar() {
-    	SonarPersister sp = new SonarPersister();
-    	sp.saveIssues();
+		saveIssues();
+	}
+	
+	//saves issues to database
+	public void saveIssues() {
+		try {
+			JsonReader jsonReader = new JsonReader();
+			JSONObject jobject = jsonReader.readJson(issuesUurl);
+			String paramPageSize = "pageSize=" + jobject.get("total").toString();
+			JSONArray issues = jsonReader.readJsonComponent(issuesUurl+paramPageSize, "issues");
+			for(int i =0; i < issues.size(); i++) {
+				JSONObject jo = (JSONObject) issues.get(i);
+				String key = jo.get("key").toString();
+				if(sonarRepo.findIssueById(key) == null) {
+					String project = jo.get("project").toString();
+					if(prijRepo.findByName(project) == null) {
+						continue;
+					}
+					String debt = "";			
+					if (jo.keySet().contains("debt"))
+						debt = jo.get("debt").toString();	
+					User user = userRepo.findByEmail(jo.get("author").toString());
+					Project prij = prijRepo.findByName(project);
+					Issue issue = new Issue(key, jo.get("severity").toString(), jo.get("component").toString(),
+							jo.get("message").toString(), debt, parseDate(jo.get("updateDate").toString()), prij, user);
+					sonarRepo.save(issue);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}		
+	
+	private java.sql.Date parseDate(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date1 = new Date(new java.util.Date().getTime());
+		try {
+			date1 =  new Date(format.parse(date.substring(0, 9)).getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return date1;
 	}
 	
 	@RequestMapping("/issue/create")
