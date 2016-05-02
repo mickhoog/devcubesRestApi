072bd643274c1edf277dd575df92726d2d1c5566
djaka <djaka>
2016-05-02 11:33:49 +0200

    update issue controller

--- src/main/java/Main/SonarResultRepository.java
+++
@@ -1,7 +0,0 @@
-package Main;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SonarResultRepository extends JpaRepository<SonarResult, String> {
-	Issue findIssueById(String id);
-}
