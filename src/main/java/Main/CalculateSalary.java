657739d88ca32ebce3d386da4080eebee3d508f4
Trane112 <harmenboerma@hotmail.com>
2016-05-17 14:22:48 +0200

    -

--- src/main/java/Main/CalculateSalary.java
+++ src/main/java/Main/CalculateSalary.java
@@ -1,35 +1,49 @@
 package Main;
 
+import Controllers.GameInfoController;
+
+import java.util.Random;
+
 /**
  * Created by Harmen on 12-5-2016.
  */
-public class CalculateSalary {
-    // Ophalen van Code Coverage, Duplications, Code Complexity, Violations, Technical Debt -> BV: [[99%], [4,6,8], [1=3,2=4,4=15,6=3], [2,4,2,55], [2d5h]]
+public class CalculateSalary{
 
-    public float CalculateSalary(){
-        float salary = (2000 * productivity()) * (calculateComplexity() * calculateCoverage() * calculateDuplication() * calculateTechnicalDebt() * calculateViolation()) + getBonus();
+    public void CalculateSalary(Object sonarPush){
+        float salary = (getBasicSalary(2) * productivity()) * (calculateComplexity() * /*calculateCoverage() **/ calculateDuplication() * calculateTechnicalDebt() * calculateViolation()) + getBonus();
 
-        return salary;
+        GameInfoController gameInfoController = new GameInfoController();
+        /*gameInfoController.changeInformation(id, money, add, salary);*/
+        //return salary;
     }
 
+    // Gebaseerd op je bv pc-level
+    private float getBasicSalary(int level){
+        float basicSalary = level * 1000;
+
+        return basicSalary;
+    }
+
+    // Random bonus
     private float getBonus(){
-        float bonus = 0;
-
-        return bonus;
+        Random random = new Random();
+        return random.nextInt(500);
     }
 
+    // Productiviteit modifier
     private float productivity(){
         float productivity = 0;
 
         return productivity;
     }
 
+    /*
     private float calculateCoverage(){
         float coverageProcent = 0;
 
         return coverageProcent;
     }
-
+*/
     private float calculateDuplication(){
         float duplicationProcent = 0;
 
