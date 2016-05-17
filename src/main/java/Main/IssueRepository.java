445aa787c27de9729ad977fc1f831f132e586483
sharelison caldera <sharelison caldera>
2016-05-17 22:33:25 +0200

    issue repo fix

--- src/main/java/Main/IssueRepository.java
+++ src/main/java/Main/IssueRepository.java
@@ -1,11 +1,6 @@
 package Main;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueRepository extends JpaRepository<Issue, String> {
-	Issue findById(String id);
-	List<Issue> findByProject_id(int id);
-	List<Issue> findByUser_id(int id);
 }
